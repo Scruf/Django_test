7dc0a6842c86cf187bf59256e6cc5cac0ad619e7
Scruf <ekozitski@hotmail.com>
2016-02-04 11:52:24 -0800

    Links are broken

--- polls/views.py
+++ polls/views.py
@@ -25,5 +25,17 @@ def results(request,question_id):
     return HttpResponse("You are looking at the result question %s"%question_id)
 
 def vote(request,question_id):
-    return HttpResponse("You are voting on question %s"%question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render (request, 'polls/detail.html',{
+            'question':question,
+            'error_message':'You did not select a choice'
+        })
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
 # Create your views here.
